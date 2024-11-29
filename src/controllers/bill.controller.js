4129ebfd77375f5ea0a7e87f1045544ec541fddf
ASC1005 <abhijeetchauhan1005@gmail.com>
2024-11-29 23:26:06 +0530

    Fix/JSON parsing for billItems

--- src/controllers/bill.controller.js
+++ src/controllers/bill.controller.js
@@ -4,7 +4,8 @@ import { asyncHandler } from "../utils/asyncHandler.util.js";
 import Bill from "../models/bill.model.js";
 
 export const createBill = asyncHandler(async (req, res, next) => {
-    const { billItems } = req.body;
+    let { billItems } = req.body;
+    billItems = JSON.parse(billItems);
     if (!billItems || billItems.length === 0) {
         throw new ApiError(400, "Bill items are required");
     }
@@ -30,21 +31,23 @@ export const getBillById = asyncHandler(async (req, res, next) => {
 
 export const updateBill = asyncHandler(async (req, res, next) => {
     const { id } = req.query;
-    const { billItems } = req.body;
+    let { billItems } = req.body;
+
+    billItems = JSON.parse(billItems);
 
     if (!Array.isArray(billItems) || billItems.length === 0) {
         throw new ApiError(400, "Invalid bill items format. It must be an array with at least one item.");
     }
 
-    const updatedBill = await Bill.findByIdAndUpdate(
-        id,
-        { $set: { billItems } }, 
-        { new: true, runValidators: true } 
-    );
+    const bill = await Bill.findById(id);
 
-    if (!updatedBill) {
+    if (!bill) {
         throw new ApiError(404, "Bill not found");
     }
+
+    bill.billItems = billItems;
+
+    const updatedBill = await bill.save();
 
     res.status(200).json(new ApiResponse(200, "Bill updated successfully", updatedBill));
 });
@@ -52,12 +55,11 @@ export const updateBill = asyncHandler(async (req, res, next) => {
 
 export const deleteBill = asyncHandler(async (req, res, next) => {
     const { id } = req.query;
-    const bill = await Bill.findById(id);
+    const bill = await Bill.findByIdAndDelete(id,{new:true});
     if (!bill) {
         throw new ApiError(404, "Bill not found");
     }
-    await bill.remove();
-    res.status(200).json(new ApiResponse(200, "Bill deleted successfully"));
+    res.status(200).json(new ApiResponse(200, "Bill deleted successfully", bill));
 });
 
 export const getBillByDate = asyncHandler(async (req, res, next) => {
