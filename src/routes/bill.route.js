916c57f7af6da3ea902be30651b4acc07e63af13
ASC1005 <abhijeetchauhan1005@gmail.com>
2024-11-29 21:02:33 +0530

    Add/Routes for bill and item

--- src/routes/bill.route.js
+++ src/routes/bill.route.js
@@ -1,6 +1,14 @@
 import { Router } from "express";
 import { asyncHandler } from "../utils/asyncHandler.util.js";
+import { createBill,getAllBills,getBillById,updateBill,deleteBill,getBillByDate } from "../controllers/bill.controller.js";
 
 const router = Router();
+
+router.route("/createBill").post(asyncHandler(createBill));
+router.route("/getAllBills").post(asyncHandler(getAllBills));
+router.route("/getBillById").post(asyncHandler(getBillById));
+router.route("/updateBill").post(asyncHandler(updateBill));
+router.route("/deleteBill").post(asyncHandler(deleteBill));
+router.route("/getBillByDate").post(asyncHandler(getBillByDate));
 
 export default router;
--- src/routes/item.route.js
+++ src/routes/item.route.js
@@ -1,5 +1,12 @@
 import { Router } from "express";
+import { getAllItems,getItemById,createItem,updateItem,deleteItem } from "../controllers/item.controller.js";
 
 const router = Router();
+
+router.route("/getAllItems").post(getAllItems);
+router.route("/getItemById").post(getItemById);
+router.route("/createItem").post(createItem);
+router.route("/updateItem").post(updateItem);
+router.route("/deleteItem").post(deleteItem);
 
 export default router;
