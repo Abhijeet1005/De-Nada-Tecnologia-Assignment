798a740ec20c5e624f1fd79a4024623dfa2c360f
ASC1005 <abhijeetchauhan1005@gmail.com>
2024-11-29 21:27:49 +0530

    Fix/Minor changes

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ dotenv.config({
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.get("/",(req,res) => {
     res.status(200).send("Healthy")
 })
